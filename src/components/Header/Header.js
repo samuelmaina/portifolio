54186b158f5bc8b925d6f01d2f9dc1b9d411afeb
samuelmayna <samuelmayna@gmail.com>
2022-06-20 12:02:01 +0300

    Coded and Added the header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
+
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from "./HeaderStyles";
+
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/samuelmaina">
+        <AiFillGithub size="4rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/samuel-maina-339a431a1/">
+        <AiFillLinkedin size="4rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
+);
+
+export default Header;
