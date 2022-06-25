d7e38905cadba8f5cdfd75a5f7a28ba68c56f904
samuelmayna <samuelmayna@gmail.com>
2022-06-25 09:15:53 +0300

    Added education components

---
+++ src/components/TimeLine/TimeLine.js
@@ -0,0 +1,168 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import { Chrono } from "react-chrono";
+
+import {
+  CarouselButton,
+  CarouselButtonDot,
+  CarouselButtons,
+  CarouselContainer,
+  CarouselItem,
+  CarouselItemImg,
+  CarouselItemText,
+  CarouselItemTitle,
+  CarouselMobileScrollNode,
+} from "./TimeLineStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Experiece } from "../../constants/constants";
+
+const TOTAL_CAROUSEL_COUNT = Experiece.length;
+
+const Timeline = () => {
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
+
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: "smooth" });
+  };
+
+  const handleClick = (e, i) => {
+    e.preventDefault();
+
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (i / Experiece.length)
+      );
+
+      scroll(carouselRef.current, scrollLeft);
+    }
+  };
+
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          Experiece.length
+      );
+
+      setActiveItem(index);
+    }
+  };
+
+  // snap back to beginning of scroll when window is resized
+  // avoids a bug where content is covered up if coming from smaller screen
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    };
+
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  const items = [
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+      cardDetailedText:
+        "On 10 May 1940, Hitler began his long-awaited offensive in the west...",
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+      cardDetailedText:
+        "On 10 May 1940, Hitler began his long-awaited offensive in the west...",
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+      cardDetailedText:
+        "On 10 May 1940, Hitler began his long-awaited offensive in the west...",
+    },
+  ];
+
+  return (
+    <Section id="education">
+      <SectionTitle>Education</SectionTitle>
+      <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
+        <>
+          {Experiece.map((item, index) => (
+            <CarouselMobileScrollNode
+              key={index}
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
+                <CarouselItemTitle>
+                  <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fill-opacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stop-color="white" />
+                        <stop
+                          offset="0.79478"
+                          stop-color="white"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
+                </CarouselItemTitle>
+                <h1>{item.school}</h1>{" "}
+                <h2 style={{ color: "white" }}>
+                  {`${item.from} - ${item.to} `}
+                </h2>
+                <br />
+                <h3>Courses Taken</h3>
+                {item.courses.map((desc, index) => {
+                  return <div key={index}>- {desc.text}</div>;
+                })}
+                <br />
+                <h3>Organization And Volunteering</h3>
+                {item.organizations.map((desc, index) => {
+                  return <div key={index}>- {desc}</div>;
+                })}
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
+        </>
+      </CarouselContainer>
+    </Section>
+  );
+};
+
+export default Timeline;
