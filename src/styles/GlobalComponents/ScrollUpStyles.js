19d4ad1cbd09b933acac9b4d63655af8fa8a5db3
samuelmayna <samuelmayna@gmail.com>
2022-08-11 14:07:39 +0300

    Made the scroll button to be on the far left

--- src/styles/GlobalComponents/ScrollUpStyles.js
+++ src/styles/GlobalComponents/ScrollUpStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const GoUpButton = styled.div`
   position: fixed;
-  left: 8%;
+  left: 4%;
   @media screen and (max-width: 678px) {
      {
       position: none;
