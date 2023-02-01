604922e3d8d5deda2fac9d5fa68dccbad408aa7b
samuelmayna <samuelmayna@gmail.com>
2023-02-01 13:01:42 +0300

    Shortened the info section

--- src/components/Hero/Info.js
+++ src/components/Hero/Info.js
@@ -16,37 +16,17 @@ export default function Info() {
           Samuel Maina W.
           <br />
         </SectionTitle>
-
         <SectionText>
-          - Learning-focused Software Engineer with BSc. Computer Science and
-          motivation to build strong career foundation alongside successful
-          team.
+          -Well versed in JavaScript, TypeScript, C++, Python,Java, SQL and
+          NoSQL databases and backend Technologies.
         </SectionText>
         <SectionText>
-          -Well versed with JavaScript, TypeScript, C++, Python,Java, SQL and
-          NoSQL databases and backend Technologies and ready to further develop
-          skills acquired from education.
-        </SectionText>
-        <SectionText>
-          -Advanced abilities in Agile methodologies and software testing.
-        </SectionText>
-        <SectionText>
-          -Always ready to learn new processes and help improve software
-          systems.
+          -Advanced abilities in Agile methodologies and software testing.Always
+          ready to learn new processes and help improve software systems.
         </SectionText>
         <SectionText>
           - Skilled in software design, testing ,debugging, deployment and
           maintenance.
-        </SectionText>
-
-        <SectionText>
-          - Passionate about software engineering. Have read many software
-          engineering books(videos and articles) and practiced many of the
-          principles and practices.
-        </SectionText>
-        <SectionText>
-          -In love with reading, anything to do with space exploration(such as
-          rocket science) and chess.
         </SectionText>
       </Section>
     </>
